9b1d2889cde38dadd9d850eb9ca8869506227905
Derekma1999 <49136343+Derekma1999@users.noreply.github.com>
2022-01-23 19:00:41 +1100

    Test

--- index.js
+++ index.js
@@ -16,6 +16,48 @@ mongoose
 // Since mongoose's Promise is deprecated, we override it with Node's Promise
 mongoose.Promise = global.Promise;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors({
+    origin: "http://localhost:3000",
+    credential: true
+}))
+
+app.use(session({
+    secret: "secretcode",
+    resave: true,
+    saveUnintialized: true
+}));
+
+app.use(cookieParser("secretcode"))
+
+app.post("/login", (req, res) => {
+    console.log(req.body);
+})
+
+app.post("/register", (req, res) => {
+    console.log(req.body);
+    User,findOne({username: req.body.username}, (err, doc) => {
+        if (err) throw err;
+        if (doc) res.send("User Already Exists");
+        if (!doc) {
+            const newUser = new User({
+                username: req.body.username,
+                password: req.body.password
+            });
+            await newUser.save();
+            res.send("User Created");
+        }
+    });
+});
+
+app.get("/user", (req, res) => {});
+
+
+
+
+
+
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header(
