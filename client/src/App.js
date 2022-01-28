bb6e3b5e2ec1874a75fbb2b02c9c2bb9c9114228
GIOO\Giooo <giorgio.bazouni@gmail.com>
2022-01-28 15:33:33 +1100

    Fixed router issue :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 //Importing Components
 import Login from "./Components/Login.js";
 
 function App() {
-    return (
-      <Router>
-        <Route path="/" exact = {true} render={()=> (
-        <div>
-          <Login/>
-          </div>
-        )}/>
-      </Router>
-    );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<div className="App">
+							<Login />
+						</div>
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
 import "./Login.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import background from "../Assets/background.svg";
 
-
-class Login extends Component {
-    render() {
-        return(
-            <div style={{backgroundImage: `url(${background})`}}>
-                <p>Hello World!</p>
-            </div>
-
-        );
-    }
+function Login() {
+	return (
+		<div style={{ backgroundImage: `url(${background})` }}>
+			<p>Hello World!</p>
+		</div>
+	);
 }
 
 export default Login;
