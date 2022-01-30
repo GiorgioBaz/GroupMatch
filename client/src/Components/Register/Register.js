498048ca438253b1cef1c4078194bf02720bcbf0
GIOO\Giooo <giorgio.bazouni@gmail.com>
2022-01-30 23:39:43 +1100

    Developed entire registration component

--- client/src/Components/Register/Register.js
+++ client/src/Components/Register/Register.js
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import Axios from "axios";
+import { Link } from "react-router-dom";
 import "./Register.css";
-import background from "../../Assets/background.svg";
+import passwordIcon from "../../Assets/password-icon.svg";
+import emailIcon from "../../Assets/email-icon.svg";
+import userIcon from "../../Assets/user-icon.svg";
 
 function Login() {
 	const [registerPassword, setRegisterPassword] = useState("");
 	const [confirmPassword, setConfirmPassword] = useState("");
 	const [registerName, setRegisterName] = useState("");
 	const [registerEmail, setRegisterEmail] = useState("");
-	const [data, setData] = useState(null);
+	//const [data, setData] = useState(null);
 
-	const register = () => {
+	const register = (e) => {
+		e.preventDefault();
 		if (registerPassword !== confirmPassword) {
 			console.log("Password is not the same");
 			return;
@@ -26,7 +30,7 @@ function Login() {
 			url: "http://localhost:5000/register",
 		}).then((res) => console.log(res));
 	};
-
+	/*
 	const getUser = () => {
 		Axios({
 			method: "GET",
@@ -38,34 +42,118 @@ function Login() {
 		});
 	};
 
+	const logout = () => {
+		Axios({
+			method: "POST",
+			withCredentials: true,
+			url: "http://localhost:5000/logout",
+		}).then((res) => console.log(res));
+		//May need a window relocation once the page is fully developed
+	};
+	*/
 	return (
-		<div className="App">
-			<div className="register">
-				<h1>Register</h1>
-				<input
-					placeholder="FullName"
-					onChange={(e) => setRegisterName(e.target.value.toString())}
-				/>
-				<input
-					placeholder="Email"
-					onChange={(e) =>
-						setRegisterEmail(
-							e.target.value.toString().toLowerCase()
-						)
-					}
-				/>
-				<input
-					placeholder="Password"
-					type={"password"}
-					onChange={(e) => setRegisterPassword(e.target.value)}
-				/>
-				<input
-					placeholder="Confirm Password"
-					type={"password"}
-					onChange={(e) => setConfirmPassword(e.target.value)}
-				/>
-				<button onClick={register}>Submit</button>
+		<div className="register-div">
+			<div className="register-card">
+				<form className="register-form" onSubmit={register}>
+					<h1 className="register-header">Register</h1>
+					<div className="name-div">
+						<div className="name-label">
+							<img
+								alt="User Icon"
+								className="user-icon"
+								src={userIcon}
+							/>
+							<h3 className="form-input-headers">Full Name</h3>
+						</div>
+						<input
+							className="input-field"
+							placeholder="Full Name"
+							onChange={(e) =>
+								setRegisterName(e.target.value.toString())
+							}
+						/>
+					</div>
+
+					<div className="email-div">
+						<div className="email-label">
+							<img
+								alt="Email Icon"
+								className="email-icon"
+								src={emailIcon}
+							/>
+							<h3 className="form-input-headers">Email</h3>
+						</div>
+						<input
+							className="input-field"
+							placeholder="Email"
+							name="email"
+							onChange={(e) =>
+								setRegisterEmail(
+									e.target.value.toString().toLowerCase()
+								)
+							}
+						/>
+					</div>
+
+					<div className="password-div">
+						<div className="password-label">
+							<img
+								alt="Password Icon"
+								className="password-icon"
+								src={passwordIcon}
+							/>
+							<h3 className="form-input-headers">Password</h3>
+						</div>
+						<input
+							className="input-field"
+							placeholder="Password"
+							type={"password"}
+							onChange={(e) =>
+								setRegisterPassword(e.target.value)
+							}
+						/>
+					</div>
+
+					<div className="confirmpass-div">
+						<div className="confirmpass-label">
+							<img
+								alt="Password Icon"
+								className="password-icon"
+								src={passwordIcon}
+							/>
+							<h3 className="form-input-headers">
+								Confirm Password
+							</h3>
+						</div>
+						<input
+							className="input-field"
+							placeholder="Confirm Password"
+							type={"password"}
+							onChange={(e) => setConfirmPassword(e.target.value)}
+						/>
+					</div>
+
+					<div className="create-button">
+						<button className="button">Create Account</button>
+					</div>
+
+					<p className="register-p">
+						Have an account?
+						<Link to="/" className="register-span">
+							Login
+						</Link>
+					</p>
+				</form>
 			</div>
+			{/*
+			<div>
+				<h1>Get User</h1>
+				<button onClick={getUser}>Submit</button>
+				{data ? <h1>Welcome Back {data.name}</h1> : null}
+			</div>
+
+			<button onClick={logout}>Log Out</button>
+			*/}
 		</div>
 	);
 }
