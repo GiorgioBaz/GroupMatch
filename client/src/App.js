7b7912b7936f5f3378dc2cb3fbafcfe5c353ee7d
GIOO\Giooo <giorgio.bazouni@gmail.com>
2022-01-30 22:40:51 +1100

    Part 1 of 2 in Merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 //Importing Components
 import Login from "./Components/Login.js";
--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,34 +1,20 @@
 import { useState } from "react";
 import Axios from "axios";
+import { Link } from "react-router-dom";
 import "./Login.css";
-import background from "../Assets/background.svg";
 import passwordIcon from "../Assets/password-icon.svg";
-import usernameIcon from "../Assets/username-icon.svg";
+import emailIcon from "../Assets/email-icon.svg";
 
 function Login() {
-	const [registerUsername, setRegisterUsername] = useState("");
-	const [registerPassword, setRegisterPassword] = useState("");
-	const [loginUsername, setLoginUsername] = useState("");
+	const [loginEmail, setLoginEmail] = useState("");
 	const [loginPassword, setLoginPassword] = useState("");
-	const [data, setData] = useState(null);
 
-	const register = () => {
+	const login = (e) => {
+		e.preventDefault();
 		Axios({
 			method: "POST",
 			data: {
-				username: registerUsername,
-				password: registerPassword,
-			},
-			withCredentials: true,
-			url: "http://localhost:5000/register",
-		}).then((res) => console.log(res));
-	};
-
-	const login = () => {
-		Axios({
-			method: "POST",
-			data: {
-				username: loginUsername,
+				email: loginEmail,
 				password: loginPassword,
 			},
 			withCredentials: true,
@@ -36,61 +22,53 @@ function Login() {
 		}).then((res) => console.log(res));
 	};
 
-	const getUser = () => {
-		Axios({
-			method: "GET",
-			withCredentials: true,
-			url: "http://localhost:5000/user",
-		}).then((res) => {
-			setData(res.data);
-			console.log(res.data);
-		});
-	};
-
-	const logout = () => {
-		Axios({
-			method: "POST",
-			data: {
-				username: loginUsername,
-				password: loginPassword,
-			},
-			withCredentials: true,
-			url: "http://localhost:5000/logout",
-		}).then((res) => console.log(res));
-		//May need a window relocation once the page is fully developed
-	};
-
 	return (
-		<div className="App" style={{ 
-			backgroundImage: `url(${background})`,
-			backgroundPosition: 'center',
-			backgroundSize: 'cover',
-			width: '100vw',
-			height: '100vh',
-			paddingTop: 0}}>
-			<div>
-				<div>
-					<form className="form-login">
-						<h1 className="form-header">Login</h1><br/><br/><br/>
-						<section>
-							<h3 className="form-input-headers" style={{marginTop: "10%"}}>Username</h3>
-							<input className="form-box"
-								style={{alignItems: "middle"}}
-								placeholder="Username"
-								onChange={(e) => setLoginUsername(e.target.value)}
-							/>
-						</section><br/><br/>
-						<section>
+		<div className="login-div">
+			<div className="login-card">
+				<form className="login-form" onSubmit={login}>
+					<h1 className="form-header">Log In</h1>
+
+					<div className="email-div">
+						<div className="email-label">
+							<img className="email-icon" src={emailIcon} />
+							<h3 className="form-input-headers">Email</h3>
+						</div>
+						<input
+							className="input-field"
+							placeholder="Email"
+							name="email"
+							onChange={(e) =>
+								setLoginEmail(
+									e.target.value.toString().toLowerCase()
+								)
+							}
+						/>
+					</div>
+
+					<div className="password-div">
+						<div className="password-label">
+							<img className="password-icon" src={passwordIcon} />
 							<h3 className="form-input-headers">Password</h3>
-							<input className="form-box"
-								placeholder="Password"
-								onChange={(e) => setLoginPassword(e.target.value)}
-							/>
-						</section>
-						<button className="button" onClick={login}>Continue</button>
-						<p style={{position: "absolute", bottom:"0px", paddingLeft:"5%"}}>I'm a new user? Register.</p>
-					</form>
-				</div>
+						</div>
+						<input
+							className="input-field"
+							placeholder="Password"
+							type={"password"}
+							onChange={(e) => setLoginPassword(e.target.value)}
+						/>
+					</div>
+
+					<div className="continue-button">
+						<button className="button">Continue</button>
+					</div>
+
+					<p className="register-p">
+						I'm a new user?
+						<Link to="/register" className="register-span">
+							Register
+						</Link>
+					</p>
+				</form>
 			</div>
 		</div>
 	);
