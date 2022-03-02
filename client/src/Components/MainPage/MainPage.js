e2d0522cfa036e899ff16b783aa86578ca3f8966
stephhills <stephanie.e.hills@student.uts.edu.au>
2022-03-02 12:31:44 +1100

    Adding elements to page without buttons

--- client/src/Components/MainPage/MainPage.js
+++ client/src/Components/MainPage/MainPage.js
@@ -2,6 +2,7 @@ import "./MainPage.css";
 import Axios from "axios";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import sampleStudent from "../../Assets/blank-profile-temp.png";
 
 
 function MainPage() {
@@ -18,13 +19,38 @@ function MainPage() {
   };
 
 	return (
-		<div>
-            <Link to="/profile"> 
+		<div className="main-div">
+			<div>
+        		<Link to="/profile"> 
 							Profile
 						</Link>    
 						
-			<button onClick={getUser}>Submit</button> 
-			{data ? <h1>{data}</h1> : null}
+				<button onClick={getUser}>Submit</button> 
+				{data ? <h1>{data}</h1> : null}
+			</div>
+
+			<div className="main-card">
+				<div className="main-student-photo">
+					<img
+						alt="Student"
+						className="main-student-photo"
+						src={sampleStudent}
+					/>
+				</div>	
+
+				<div className="student-info">
+					<div className="student-title">
+						<h3 className="student-name">Student Name</h3>
+						<h5 className="student-degree">Degree</h5>
+					</div>
+
+					<div className="student-academics">
+						<p>Subject A</p>
+						<p>Subject B</p>
+						<p>Subject C</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
