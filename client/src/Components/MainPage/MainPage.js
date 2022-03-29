6b1bd0e9f865ed48a7499985755414a2ef452860
stephhills <stephanie.e.hills@student.uts.edu.au>
2022-03-29 17:15:59 +1100

    Added user data to main page

--- client/src/Components/MainPage/MainPage.js
+++ client/src/Components/MainPage/MainPage.js
@@ -1,12 +1,26 @@
 import "./MainPage.css";
 import Axios from "axios";
-import { useState } from "react";
+import { useState, useEffect, } from "react";
+import Swal from "sweetalert2";
 import { Link } from "react-router-dom";
+
 import sampleStudent from "../../Assets/blank-profile-temp.png";
 import rejectIcon from "../../Assets/reject-button-icon.svg";
 import interestIcon from "../../Assets/interest-button-icon.svg";
 
 function MainPage() {
+	const [username, setUsername] = useState("");
+	const [grade1, setGrade1] = useState("HD");
+	const [subject1, setSubject1] = useState("");
+	const [grade2, setGrade2] = useState("HD");
+	const [subject2, setSubject2] = useState("");
+	const [grade3, setGrade3] = useState("HD");
+	const [subject3, setSubject3] = useState("");
+	const [degree, setDegree] = useState("");
+	const [gpa, setGpa] = useState("");
+	const [studyLoad, setStudyLoad] = useState("");
+	const [avatar, setAvatar] = useState("");
+
 	const { data, setData } = useState(null);
 
 	async function getUserInfo() {
@@ -17,6 +31,38 @@ function MainPage() {
 		});
 		return user.data.user;
 	}
+	function setUserInfo() {
+		Axios({
+			method: "GET",
+			withCredentials: true,
+			url: "http://localhost:5000/userProfile",
+		}).then((res) => {
+			if (res.data.success) {
+				const academics = res.data.user.academics;
+				setUsername(res.data.user.name);
+				setGrade1(academics[0]?.grade);
+				setSubject1(academics[0]?.subject);
+				setGrade2(academics[1]?.grade);
+				setSubject2(academics[1]?.subject);
+				setGrade3(academics[2]?.grade);
+				setSubject3(academics[2]?.subject);
+				setDegree(res.data.user.degree);
+				setGpa(res.data.user.gpa);
+				setStudyLoad(res.data.user.studyLoad);
+				setAvatar(res.data.user.avatar);
+			} else {
+				Swal.fire(
+					"Oops! Something Broke",
+					`${res.data.message}`,
+					"error"
+				);
+			}
+		});
+	}
+
+	useEffect(() => {
+		setUserInfo();
+	}, []);
 
 	return (
 		<div className="main-div">
@@ -32,29 +78,29 @@ function MainPage() {
 					<img
 						alt="Student"
 						className="main-student-photo"
-						src={sampleStudent}
+						src={avatar}
 					/>
 				</div>
 
 				<div className="student-info">
 					<div className="student-title">
-						<h3 className="student-name">Student Name</h3>
-						<h5 className="student-degree">Degree</h5>
+						<h3 className="student-name">{username}</h3>
+						<h5 className="student-degree">{degree}</h5>
 					</div>
 
 					<div className="student-academics">
-						<p><span class="academic-marks"> HD/D/C/F - </span>Subject A</p>
-						<p><span class="academic-marks"> HD/D/C/F - </span>Subject B</p>
-						<p><span class="academic-marks"> HD/D/C/F - </span>Subject C</p>
+						<p><span className="academic-marks">{grade1 || "HD"} - </span>{subject1}</p>
+						<p><span className="academic-marks">{grade2 || "HD"} - </span>{subject2}</p>
+						<p><span className="academic-marks">{grade3 || "HD"} - </span>{subject3}</p>
 					</div>
 
 					<div className="student-degree-details">
 						<div className="student-gpa">
-							<p>GPA/7</p>
+							<p>{gpa}</p>
 						</div>
 
 						<div className="student-study">
-							<p>Full/Part Time</p>
+							<p>{studyLoad}</p>
 						</div>
 					</div>
 				</div>
