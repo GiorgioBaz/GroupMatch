099fe40528668f1a805379851a8ff7dc6ce016f0
GiorgioBaz <giorgio.bazouni@gmail.com>
2023-11-19 13:18:19 +1100

    Changed config file in attempt to deploy on 'render'

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: "https://groupmatchapp.herokuapp.com/api",
+    baseURL: "https://groupmatch.onrender.com/api",
 });
